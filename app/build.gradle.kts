1beda8f32f8b1cc60b43e003bf92267535cb0b04
tomchccom <dreamkms2014@gmail.com>
2026-01-10 16:52:39 +0900

    chore: Gradle 빌드 설정 정리

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,7 +54,6 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.recyclerview:recyclerview:1.3.2")
-    implementation("androidx.viewpager2:viewpager2:1.1.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
